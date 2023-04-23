3a047c00f4f9a1e438faf9464d943a0a46d6e745
Vadim <vadimgol07@inbox.ru>
2023-04-23 15:24:28 +0300

    kurs

---
+++ phyton_trening/kurs.py
@@ -0,0 +1,15 @@
+#a: list[1, 2, 3, 4] - бакалавр
+#b: list[5, 6] - магистр
+#c: list[7, 8, 9] - аспирант
+
+a = 3
+def kurs_1(a):
+    if a == 1 or a == 2 or a ==3 or a == 4:
+        print('Бакалавр')
+    elif a == 5 or a == 6:
+        print('Магистр')
+    elif a == 7 or a ==8 or a == 9:
+        print('Аспирант')
+    else:
+        print('Введите корректный курс')
+kurs_1(a)
